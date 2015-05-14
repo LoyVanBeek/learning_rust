74bd3dca92d2473182154c536c86b274c52494d9
Loy van Beek <loy.vanbeek@gmail.com>
2015-05-14 12:09:46 +0200

    Created a Base-trait that can be implemented for several types of base

---
+++ robot_skills/src/lib.rs
@@ -0,0 +1,12 @@
+extern crate geometry_msgs;
+
+trait Base {
+    fn base_move(&self, goal: geometry_msgs::PoseStamped);
+    fn force_drive(&self, direction: geometry_msgs::Twist);
+    fn get_location(&self) -> geometry_msgs::PoseStamped;
+
+    fn set_initial_pose(&self, pose: geometry_msgs::PoseStamped);
+    fn go(&self, goal: geometry_msgs::PoseStamped, timeout: i32);
+    fn reset_costmap(&self);
+    fn cancel_goal(&self);
+}

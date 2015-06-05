f465588c86ce2212c732477d4593726171349b2d
Loy van Beek <loy.vanbeek@gmail.com>
2015-06-05 21:13:49 +0200

    Using BaseResult for Base

--- robot_skills/src/amigo_base.rs
+++ robot_skills/src/amigo_base.rs
@@ -1,11 +1,20 @@
 extern crate geometry_msgs;
 
+#[derive(Debug)]
+enum BaseError {
+    HardwareFail,
+    EnvironmentCollision,
+    GoalUnreachable,
+}
+
+pub type BaseResult = Result<geometry_msgs::PoseStamped, BaseError>;
+
 trait Base {
-    fn base_move(& mut self, goal: geometry_msgs::PoseStamped);
-    fn force_drive(& mut self, direction: geometry_msgs::Twist);
+    fn base_move(& mut self, goal: geometry_msgs::PoseStamped) -> BaseResult;
+    fn force_drive(& mut self, direction: geometry_msgs::Twist) -> BaseResult;
     fn get_location(& mut self) -> geometry_msgs::PoseStamped;
     fn set_initial_pose(& mut self, pose: geometry_msgs::PoseStamped);
-    fn go(& mut self, goal: geometry_msgs::PoseStamped, timeout: i32);
+    fn go(& mut self, goal: geometry_msgs::PoseStamped, timeout: i32) -> BaseResult;
     fn reset_costmap(& mut self);
     fn cancel_goal(& mut self);
 }
@@ -17,12 +26,14 @@ struct AmigoBase
 }
 
 impl Base for AmigoBase {
-    fn base_move(& mut self, goal: geometry_msgs::PoseStamped){
+    fn base_move(& mut self, goal: geometry_msgs::PoseStamped) -> BaseResult{
         self.current_position = goal;
+        return Ok(self.current_position);
     }
 
-    fn force_drive(& mut self, direction: geometry_msgs::Twist){
+    fn force_drive(& mut self, direction: geometry_msgs::Twist) -> BaseResult{
         self.current_position.pose = self.current_position.pose + direction;
+        return Ok(self.current_position);
     }
 
     fn get_location(& mut self) -> geometry_msgs::PoseStamped {
@@ -33,8 +44,8 @@ impl Base for AmigoBase {
         self.current_position = pose;
     }
 
-    fn go(& mut self, goal: geometry_msgs::PoseStamped, timeout: i32){
-        self.base_move(goal);
+    fn go(& mut self, goal: geometry_msgs::PoseStamped, timeout: i32) -> BaseResult{
+        return self.base_move(goal);
     }
 
     fn reset_costmap(& mut self){}
